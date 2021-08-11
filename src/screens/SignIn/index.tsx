7b8471a8fcd229cb0cf082844979757a8b1ffb4c
Jose Murillo <jooseemurillo@gmail.com>
2021-08-11 20:56:27 -0300

    chore: create page signIn

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,10 +1,76 @@
-import React from 'react-native'
-import { Container } from './styles';
+import React from 'react';
+import {  Image, StatusBar, View } from 'react-native';
 
-export function SignIn() {
+import logoPocketDiet from '../../global/assets/logoPocketDiet.png';
+import PlantUp from '../../global/assets/PlantUp.png';
+import PlantDown from '../../global/assets/PlantDown.png';
+
+import {Input} from '../../components/Input';
+
+import {
+  Container,
+  Form,
+  Title,
+  Description,
+  ForgotPassword,
+  ForgotPasswordText,
+  PlantImageBottom,
+  PlantImageTop,
+  Header,
+  Main,
+  LabelInput,
+  ContainerButton
+} from './styles';
+import { Button } from '../../components/Button';
+
+export const SignIn: React.FC = () => {
   return (
-    <Container>
-      <h1>Hello, signIn</h1>
-    </Container>
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="#FDFDFD"/>
+      <Container>
+        <PlantImageTop source={PlantUp} />
+        <Header>
+          <Image source={logoPocketDiet}/>
+        </Header>
+        <Main>
+          <Form>
+            <Title>
+              Entre com sua conta
+            </Title>
+            <Description>
+              Realize seu acesso com os dados informados pelo profissional.
+            </Description>
+
+            <View>
+              <LabelInput>Email</LabelInput>
+              <Input 
+                placeholder="E-mail" 
+                type="sms"
+              />
+            </View>
+
+            <View style={{marginTop: 16}}>
+              <LabelInput>Senha</LabelInput>
+              <Input 
+                placeholder="Senha" 
+                secureTextEntry={true}
+                type="lock"
+              />
+            </View>
+
+            <ForgotPassword>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+
+            <ContainerButton>
+              <Button buttonText="Entrar" />
+            </ContainerButton>
+          </Form>
+        </Main>
+        <PlantImageBottom>
+          <Image source={PlantDown} />
+        </PlantImageBottom>
+      </Container>
+    </>
   );
 }
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,6 +1,90 @@
 import styled from 'styled-components/native'
 
-export const Container = styled.View`
-  
+export const ContainerButton = styled.View`
+  margin-top: 24px;
 `;
 
+export const LabelInput = styled.Text`
+  font-size: 12px;
+  line-height: 14px;
+  color: ${({theme}) => theme.colors.text};
+  font-family: ${({theme}) => theme.fonts.regular};
+  margin-bottom: 4px;
+`;
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #FDFDFD;
+
+  padding: 0 24px;
+`;
+
+export const Header = styled.View`
+  margin-top: 40px;
+`;
+
+export const Main = styled.View`
+  flex: 1;
+  align-items: center;
+`;
+
+export const Form = styled.View`
+  width: 100%;
+  padding: 70px 24px;
+  padding-bottom: 68px;
+  z-index: 10;
+`;
+export const Title = styled.Text`
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 28px;
+  color: ${({theme}) => theme.colors.text};
+  font-family: ${({theme}) => theme.fonts.bold};
+`;
+export const Description = styled.Text`
+  width: 235px;
+  font-size: 12px;
+  line-height: 14px;
+  margin-top: 5px;
+  margin-bottom: 24px;
+  color: ${({theme}) => theme.colors.text};
+`;
+
+
+export const InputContainer = styled.View`
+  width: 318px;
+  height: 54px;
+  border: 1px solid #406334;
+  background: #F0F0F0;
+  border-radius: 8px;
+`;
+
+export const Input = styled.TextInput`
+
+`;
+export const ContainerInput = styled.View`
+`;
+export const Label = styled.Text``;
+
+export const ForgotPassword = styled.View`
+  width: 100%;
+  align-items: flex-end;
+`;
+export const ForgotPasswordText = styled.Text`
+  font-size: 12px;
+  line-height: 14px;
+  color: ${({theme}) => theme.colors.text};
+  font-family: ${({theme}) => theme.fonts.medium};
+  margin-top: 4px;
+`;
+
+export const PlantImageTop = styled.Image`
+  position: absolute; 
+  right: 12%; 
+  top: 0%;
+`;
+export const PlantImageBottom = styled.View`
+  position: absolute; 
+  left: 0%;
+  bottom: 7%;
+`;
