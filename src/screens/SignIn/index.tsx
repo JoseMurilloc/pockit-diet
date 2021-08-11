82f6b6b3949eb6d61e064d6acbed8d30a0fe9e7d
Jose Murillo <jooseemurillo@gmail.com>
2021-08-11 10:49:15 -0300

    🎉 commit initial

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react-native'
+import { Container } from './styles';
+
+export function SignIn() {
+  return (
+    <Container>
+      <h1>Hello, signIn</h1>
+    </Container>
+  );
+}
