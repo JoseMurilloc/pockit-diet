d553b652acde467d02d5009d857c58df7ae68b15
Jose Murillo <jooseemurillo@gmail.com>
2021-08-12 18:07:57 -0300

    chore: update new colors of theme

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -4,8 +4,9 @@ export default {
 
     shape: '#FFFFFF',
     text: '#131313',
+    text_light: 'rgba(20, 20, 20, 0.5)',
     text_dark: '#000000',
-    background: '#F0F2F5',
+    background: '#FDFDFD',
   },
   fonts: {
     regular: 'Raleway_400Regular',
