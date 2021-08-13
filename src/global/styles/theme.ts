0a99ce710a5b220ddc09c28237268b01c72c389b
Jose Murillo <jooseemurillo@gmail.com>
2021-08-13 19:46:23 -0300

    chore: colors and assets

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -2,11 +2,20 @@ export default {
   colors: {
     primary: '#74A065',
 
-    shape: '#FFFFFF',
+    shape: '#F0F0F0',
+    background: '#FDFDFD',
+
     text: '#131313',
     text_light: 'rgba(20, 20, 20, 0.5)',
     text_dark: '#000000',
-    background: '#FDFDFD',
+
+    error: '#FF8080',
+    error_light: 'rgba(255, 179, 179, 0.5)',
+    error_text: '#FF7575',
+
+    success: '#406334', 
+    success_light: 'rgba(116, 160, 101, 0.2)',
+    success_text: '#26491A',
   },
   fonts: {
     regular: 'Raleway_400Regular',
