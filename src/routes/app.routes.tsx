e0ff0e6da88a2c638537d0a0d687148d6ed3d941
Jose Murillo <jooseemurillo@gmail.com>
2021-08-17 15:59:27 -0300

    chore: using material icons on tabBar

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,25 +1,15 @@
 import React from 'react' 
 
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import { MaterialIcons } from '@expo/vector-icons'
+import { useTheme } from 'styled-components'
 
 import Initial from '../screens/App/Initial'
 import Revenues from '../screens/App/Revenues'
-import { useTheme } from 'styled-components'
-import { Image } from 'react-native'
-
-import homeIcon from  '../global/assets/icons/tabBar/home.png'
-import homeIconActive from  '../global/assets/icons/tabBar/home_active.png'
-import bookIcon from  '../global/assets/icons/tabBar/book.png'
-import bookIconActive from  '../global/assets/icons/tabBar/book_active.png'
-import profileIcon from  '../global/assets/icons/tabBar/profile.png'
-import profileIconActive from  '../global/assets/icons/tabBar/profile_active.png'
-import replacementIcon from  '../global/assets/icons/tabBar/replacement.png'
-// import replacementIconActive from  '../global/assets/icons/tabBar/replacement_active.png'
 
 
 import Replacement from '../screens/App/Replacement'
 import Profile from '../screens/App/Profile'
-
 
 const Tab = createBottomTabNavigator()
 
@@ -48,25 +38,33 @@ export function AppRoutes() {
         name="Início"
         component={Initial}
         options={{
-          tabBarIcon: ({focused, color, size}) => {
-            if (focused) {
-              return <Image source={homeIconActive}/>
-            }
-            return <Image source={homeIcon} />
-          }
+          tabBarIcon: ({focused, color, size}) => (
+            <MaterialIcons 
+              name="home" 
+              size={26} 
+              color={focused ?  
+                theme.colors.primary_strong 
+                : theme.colors.text_super_light
+              } 
+            />
+          )
         }}
       />
-      
+
       <Tab.Screen 
         name="Receitas"
         component={Revenues}
         options={{
-          tabBarIcon: ({focused, color, size}) => {
-            if (focused) {
-              return <Image source={bookIconActive}/>
-            }
-            return <Image source={bookIcon} />
-          }
+          tabBarIcon: ({focused, color, size}) => (
+            <MaterialIcons 
+              name="local-dining" 
+              size={26} 
+              color={focused ?  
+                theme.colors.primary_strong 
+                : theme.colors.text_super_light
+              } 
+            />
+          )
         }}
       />
 
@@ -74,12 +72,16 @@ export function AppRoutes() {
         name="Substituição"
         component={Replacement}
         options={{
-          tabBarIcon: ({focused, color, size}) => {
-            if (focused) {
-              return <Image source={bookIconActive}/>
-            }
-            return <Image source={replacementIcon} />
-          }
+          tabBarIcon: ({focused, color, size}) => (
+            <MaterialIcons 
+              name="menu-book" 
+              size={26} 
+              color={focused ?  
+                theme.colors.primary_strong 
+                : theme.colors.text_super_light
+              } 
+            />
+          )
         }}
       />
 
@@ -87,12 +89,16 @@ export function AppRoutes() {
         name="Perfil"
         component={Profile}
         options={{
-          tabBarIcon: ({focused, color, size}) => {
-            if (focused) {
-              return <Image source={profileIconActive}/>
-            }
-            return <Image source={profileIcon} />
-          }
+          tabBarIcon: ({focused, color, size}) => (
+            <MaterialIcons 
+              name="person" 
+              size={26} 
+              color={focused ?  
+                theme.colors.primary_strong 
+                : theme.colors.text_super_light
+              } 
+            />
+          )
         }}
       />
     </Tab.Navigator>
