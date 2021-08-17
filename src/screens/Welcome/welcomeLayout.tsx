2e79a21210271b76fdc90874b68711b35e25f0f4
Jose Murillo <jooseemurillo@gmail.com>
2021-08-17 15:26:44 -0300

    chore: ui add images of page welcome

--- src/screens/Welcome/welcomeLayout.tsx
+++ src/screens/Welcome/welcomeLayout.tsx
@@ -4,7 +4,11 @@ import { WelcomeProps } from './welcomeModal';
 
 import { ProgressBarWelcome } from '../../components/ProgressBar';
 import { Button } from '../../components/Button';
-import WelcomeImage from '../../global/assets/welcome.png';
+import welcomeImage from '../../global/assets/welcome.png';
+import welcomeRevenuesImage from '../../global/assets/welcomeRevenues.png';
+import welcomeWorkoutImage from '../../global/assets/welcomeWorkout.png';
+
+import theme from '../../global/styles/theme'
 
 export function WelcomeLayout ({
   data: {MAX_PAGE_COUNT, contentMessages, indexPage, progress, handleNextPage}
@@ -22,19 +26,31 @@ export function WelcomeLayout ({
       </Styles.Header>
 
       <Styles.ContentPicture>
-        <Styles.ImageWelcome source={WelcomeImage}/>
+        {indexPage === 0  && (
+          <Styles.ImageWelcome source={welcomeImage}/>  
+        )}
+        {indexPage === 1  && (
+          <Styles.ImageWelcome source={welcomeRevenuesImage}/>  
+        )}
+        {indexPage === 2  && (
+          <Styles.ImageWelcome source={welcomeWorkoutImage}/>  
+        )}
       </Styles.ContentPicture>
 
       <Styles.ContentMain>
-        <Styles.Title>{contentMessages[indexPage].title}</Styles.Title>
-        <Styles.Message>{contentMessages[indexPage].message}</Styles.Message>
+        <Styles.Title>
+          {contentMessages[indexPage].title}
+        </Styles.Title>
+        <Styles.Message>
+          {contentMessages[indexPage].message}
+        </Styles.Message>
 
         <Styles.ContentProgressBar>
           <ProgressBarWelcome 
             progress={progress}
             styles={{
               height: 11,
-              color: "#37582b",
+              color: theme.colors.success_text,
               borderRadius: 8,
               width: 325
             }}
